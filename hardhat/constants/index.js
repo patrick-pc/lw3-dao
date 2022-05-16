b3f7718fef054700c296adfc966325c6a86da663
Peter Parker <web3slinger@gmail.com>
2022-05-16 10:48:05 -0500

    Build crypto devs dao contract

---
+++ hardhat/constants/index.js
@@ -0,0 +1,5 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+  '0xFA72242a74d4c874336D0ee61acd15909B56d2c8'
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS }
